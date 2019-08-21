44c7e29c47e4ff4c6b3436998ee85acad1335c88
ariel <arielgordon123@gmail.com>
2019-08-21 17:03:13 +0300

    add script6

--- vagrant/fixScripts/exercise6-fix.sh
+++ vagrant/fixScripts/exercise6-fix.sh
@@ -10,15 +10,16 @@ fi
 
 # check on which server the script is running 
 server=$(hostname)
-
 [[ $server = "server1" ]] && host="server2" || host="server1"
+
 # the dest folder is the lasp parametr
 dest_folder=${@: -1}
+
 # files is all args exept from last
 files=${@:1:$#-1}
+
 # claculate size
 size=$(ls -l $files | awk '{sum += $5} END {print sum}')
 
-script -c "scp $files vagrant@$host:$dest_folder" > /tmp/test.txt
-
-echo $size
+# copy files to dest host and print size in bytes
+script -q -c "scp $files vagrant@$host:$dest_folder" > /dev/null && echo $size
