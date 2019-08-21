b6b0ad826771fb789ab59af4bb419fdd10ade78e
ariel <arielgordon123@gmail.com>
2019-08-21 16:38:54 +0300

    add script6

--- vagrant/fixScripts/exercise6-fix.sh
+++ vagrant/fixScripts/exercise6-fix.sh
@@ -1,2 +1,24 @@
 #!/bin/bash
 #add fix to exercise6-fix here
+
+
+# validate num of args
+if [ "$#" -lt 2 ]; then
+    echo "Please provide atleast 2 args"
+    exit 2
+fi
+
+# check on which server the script is running 
+server=$(hostname)
+
+[[ $server = "server1" ]] && host="server2" || host="server1"
+# the dest folder is the lasp parametr
+dest_folder=${@: -1}
+# files is all args exept from last
+files=${@:1:$#-1}
+# claculate size
+size=$(ls -l $files | awk '{sum += $5} END {print sum}')
+
+script -c "scp $files vagrant@$host:$dest_folder" > /tmp/test.txt
+
+echo $size
